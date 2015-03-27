f1d4e0bdbdd6ceaa94a0f3be148c51b5948deb73
Mike Knueppel <mikeknueppel@hotmail.com>
2015-03-27 14:23:41 -0700

    Refactor Stack specs

--- spec/src/stores/stack-spec.js
+++ spec/src/stores/stack-spec.js
@@ -1,18 +1,24 @@
 var expect = require('chai').expect;
 var Stack = require('../../../src/stores/stack.js');
 
+var initializeStack = function() {
+  var elements = ['1st', '2nd', '3rd'];
+  elements.forEach(function(element, index, array) {
+    Stack.push(element);
+  });
+};
+
 describe('Stack', function() {
   beforeEach(function() {
     Stack.clear();
   });
 
   describe('#load()', function() {
-    it('returns the elements in the stack', function() {
-      var elements = ['1st', '2nd', '3rd'];
-      elements.forEach(function(element, index, array) {
-        Stack.push(element);
-      });
+    beforeEach(function() {
+      initializeStack();
+    });
 
+    it('returns the elements in the stack', function() {
       expect(Stack.load()).to.eql(['3rd', '2nd', '1st']);
     });
   });
@@ -21,8 +27,7 @@ describe('Stack', function() {
     it('pushes an element onto the stack', function() {
       var element = 'an element';
       Stack.push(element);
-      stack = Stack.load();
-      expect(stack).to.have.length(1);
+      expect(Stack.load()).to.have.length(1);
     });
   });
 });
