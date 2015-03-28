c5a125c5a640fc7062a89fafde3d34bfa7991d25
Mike Knueppel <mikeknueppel@hotmail.com>
2015-03-27 18:54:05 -0700

    Add assertions for push, pop, and clear functions

--- spec/src/stores/queue-spec.js
+++ spec/src/stores/queue-spec.js
@@ -1,6 +1,5 @@
 var expect = require('chai').expect;
 var Queue = require('../../../src/stores/queue');
-// var queue = Queue.load();
 
 var initializeQueue = function() {
   var elements = ['1st', '2nd', '3rd'];
@@ -10,10 +9,34 @@ var initializeQueue = function() {
 };
 
 describe('Queue', function() {
+  beforeEach(function() {
+    Queue.clear();
+    initializeQueue();
+  });
   describe('#load()', function() {
     it('returns the elements in the queue in the proper order', function() {
-      initializeQueue();
       expect(Queue.load()).to.eql(['1st', '2nd', '3rd']);
+    });
+  });
+
+  describe('#push()', function() {
+    it('pushes the element to the back of the queue', function() {
+      Queue.push('element');
+      expect(Queue.load()).to.eql(['1st', '2nd', '3rd', 'element']);
+    });
+  });
+
+  describe('#pop()', function() {
+    it('pops an element from the front of the queue', function() {
+      Queue.pop();
+      expect(Queue.load()).to.eql(['2nd', '3rd']);
+    });
+  });
+
+  describe('#clear()', function() {
+    it('clears the queue', function() {
+      Queue.clear();
+      expect(Queue.load()).to.have.length(0);
     });
   });
 });
