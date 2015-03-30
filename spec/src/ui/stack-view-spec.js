b4de0dc796c0f793f5798b56bd854124cb08e162
Mike Knueppel <mikeknueppel@hotmail.com>
2015-03-30 08:46:09 -0700

    Add assertions

--- spec/src/ui/stack-view-spec.js
+++ spec/src/ui/stack-view-spec.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
-var spies = require('chai-spies');
-chai.use(spies);
+// var spies = require('chai-spies');
+// chai.use(spies);
 var expect = chai.expect;
 var $ = require('jquery');
 var StackView = require('ui/main-view');
@@ -17,6 +17,10 @@ var unRenderStackView = function() {
 describe('Stack UI', function() {
   beforeEach(function() {
     renderStackView();
+    $newElement = $('.new-element');
+    $pushElementButton = $('.add-element');
+    $popElementButton = $('.pop');
+    $clearButton = $('.clear');
   });
 
   afterEach(function() {
@@ -27,28 +31,43 @@ describe('Stack UI', function() {
     expect(StackView).to.be.ok;
   });
 
-  it('has an input field for new elements', function() {
-    expect($('.new-element').length).to.equal(1);
+  it('has an input, a button to push an element, pop an element, and clear', function() {
+    expect($newElement.length).to.equal(1);
+    expect($pushElementButton.length).to.equal(1);
+    expect($popElementButton.length).to.equal(1);
+    expect($clearButton.length).to.equal(1);
   });
 
-  describe('button click: add', function() {
-    var handleClickEvent;
-    var handler;
+  it('has an input field for new elements', function() {
+    expect($newElement.length).to.equal(1);
+  });
+
+  describe('Button click: Push', function() {
+    beforeEach(function() {
+      Stack = require('../../../src/stores/stack');
+      Stack.clear();
+    });
 
     describe('when a valid element is entered', function() {
       beforeEach(function() {
-        handleClickEvent = function(event) {};
-        handler = chai.spy(handleClickEvent);
+        $newElement.val('hello');
+        $pushElementButton.click();
       });
 
-      it('adds the new element to the stack', function() {
+      it('adds the element to the list of elements', function() {
+         expect($('.list-of-elements li')).to.have.length(1);
+      });
+    });
 
+    describe('when an invalid element is entered', function() {
+      beforeEach(function() {
+        $newElement.val('');
+        $pushElementButton.click();
       });
 
-      // it('calls the appropriate handler', function() {
-      //   expect(handler).to.have.been.called.once;
-      // });      
-      // it('clears the input text box'...)
+      it('does not add the element', function() {
+        expect($('.list-of-elements li')).to.have.length(0);
+      })
     });
   });
 });
